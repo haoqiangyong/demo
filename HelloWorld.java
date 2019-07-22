2a81aae8e34298420d2293ced31d128cc11c4161
haoqiangyong <2443357274qq.com>
2019-07-22 17:53:27 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,32 @@
+public class HelloWorld
+
+{
+
+
+ //程序的主函数入门
+
+
+ public staticvoid main(String args[])
+
+
+ {
+
+//输出打印字符语句
+
+
+System.out.println("HelloWorld");
+
+
+System.out.println("1");
+
+
+System.out.println("HelloWorld！");
+
+
+System.out.println("2");
+
+
+
+ }
+
+}
